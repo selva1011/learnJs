00b99fce64e0b71176ae386da807a107bc16959e
Selva Ganapathi <60595640+selva1011@users.noreply.github.com>
2024-08-15 10:14:14 +0530

    icon change tab

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="https://www.svgrepo.com/show/349419/javascript.svg" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
